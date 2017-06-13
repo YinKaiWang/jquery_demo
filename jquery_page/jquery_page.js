751c893a4fc14b96593fb8c28e052743b4881fdc
YinKaiWang <939457113@qq.com>
2017-06-13 10:48:00 +0800

    操作jquery的show和hide方法

---
+++ jquery_page/jquery_page.js
@@ -0,0 +1,25 @@
+require.config({
+	baseUrl: "../public",
+	paths:{
+		jquery: "jquery-1.8.2.min",
+	}
+});
+require(['jquery'],function($){
+	// show & hide
+	$("#s_h").on('click','input',function(event) {
+		event.preventDefault();
+		var $this=$(this),
+			txt=$this.val(),
+			speed=1000,
+			ele=$("#s_h_module");
+		if(txt=="show"){
+			ele.show(speed, function() {
+				ele.html(txt+"时间:"+speed);
+			});
+		}else{
+			ele.hide('speed', function() {
+				alert(txt+"时间:"+speed);
+			});
+		}
+	});
+})
